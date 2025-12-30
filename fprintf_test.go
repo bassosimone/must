cca6059289e83827044a430b52681dccf1a57fcd
Simone Basso <bassosimone@gmail.com>
2025-12-30 23:35:35 +0100

    feat: make sure we panic on failure

--- fprintf_test.go
+++ fprintf_test.go
@@ -3,6 +3,8 @@
 package must_test
 
 import (
+	"errors"
+	"io"
 	"strings"
 	"testing"
 
@@ -10,8 +12,30 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMustFprintf(t *testing.T) {
+func TestMustFprintfSuccess(t *testing.T) {
 	var sb strings.Builder
 	must.Fprintf(&sb, "%s\n", "Hello, world")
 	assert.Equal(t, "Hello, world\n", sb.String())
+}
+
+type flexibleWriter struct {
+	WriteFn func(data []byte) (int, error)
+}
+
+var _ io.Writer = flexibleWriter{}
+
+// Write implements [io.Writer].
+func (f flexibleWriter) Write(data []byte) (int, error) {
+	return f.WriteFn(data)
+}
+
+func TestMustFprintfFailure(t *testing.T) {
+	writer := flexibleWriter{
+		WriteFn: func(data []byte) (int, error) {
+			return 0, errors.New("mocked error")
+		},
+	}
+	assert.Panics(t, func() {
+		must.Fprintf(writer, "%s\n", "Hello, world")
+	})
 }
